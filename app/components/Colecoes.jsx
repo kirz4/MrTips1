7fa7a79dc3fa2163f53cf0b6d8e1c186d5b63de3
Felipe Lima <pegasusyt1@gmail.com>
2024-05-08 15:11:54 -0300

    mudanca da cor dos textos

--- app/components/Colecoes.jsx
+++ app/components/Colecoes.jsx
@@ -8,10 +8,10 @@ const Coleções = () => {
   return (
     <section id="colecoes">
       <div class="py-6 sm:py-8 lg:py-12">
-        <h1 className=" flex justify-center items-center text-5xl font-bold text-white">
+        <h1 className=" flex justify-center items-center text-5xl font-bold text-[#a6d232] drop-shadow-[0_1.8px_1.8px_rgba(139,69,19,1)] sm:mb-10">
           Coleções
         </h1>
-        <div class="mx-auto max-w-screen-2xl px-4 md:px-8 sm:pt-10">
+        <div class="mx-auto max-w-screen-2xl px-4 md:px-8 mt-16">
           <div class="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12" />
 
           <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
@@ -88,7 +88,7 @@ const Coleções = () => {
             {/*<!-- image - end -->*/}
           </div>
         </div>
-        <h1 className=" flex justify-center items-center text-5xl font-bold text-white mt-24">
+        <h1 className=" flex justify-center items-center text-5xl font-bold text-[#a6d232] drop-shadow-[0_1.8px_1.8px_rgba(139,69,19,1)] mt-24">
           Estoque
         </h1>
       </div>
